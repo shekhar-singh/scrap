7d258bdb68119ad54a69e92ac7c7c1c2fc51e087
shekhar-singh <sschoudhary60@gmail.com>
2015-06-22 21:12:24 +0530

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python 
+
+import requests
+from bs4 import BeautifulSoup
+
+uri = requests.get("http://video9.in/english/")
+soup=BeautifulSoup(url.text)
+for link in soup.find_all("div",{"class": "updates"}):
+    print link.text
